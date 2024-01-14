8e95647c0654847436a1e43334a5d39d2bb72d82
Tosty <ajaygimbhal@gmail.com>
2024-01-14 00:06:58 -0800

    Initial commit

---
+++ mylibrary/src/main/java/com/example/mylibrary/Toaster.java
@@ -0,0 +1,14 @@
+package com.example.mylibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import androidx.annotation.Keep;
+
+@Keep
+public class Toaster {
+    public static void make(Context context, String msg){
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+    }
+
+}
